30bc32b5a088debf8ee2f0ab7f7db79b36c6d08b
Andy D <mindovermiles262@gmail>
2019-03-11 12:38:49 -0700

    initial deploy

--- darkMode.js
+++ darkMode.js
@@ -1,30 +1,35 @@
-const bgColor = "#121212";
-const bgAccent = "#1a1a1a"
-const fontColor = "#b3b3b3"
-const headerColor = "#ffffff"
+const darkMode = function() {
+  const bgColor = "#121212";
+  const bgAccent = "#1a1a1a"
+  const fontColor = "#b3b3b3"
+  const headerColor = "#ffffff"
 
-// Set BG Color
-document.body.style.backgroundColor = bgColor;
-document.querySelectorAll(".navbar, .card-main, #footer").forEach( (el) => { 
-  el.style.background = bgColor
-});
+  // Set BG Color
+  document.body.style.backgroundColor = bgColor;
+  document.querySelectorAll(".navbar, .card-main, #footer").forEach( (el) => { 
+    el.style.background = bgColor
+  });
 
-// Set BG Accent Color
-document.querySelectorAll(".lesson-functions, .course-card, .gradient, .lesson-content__panel").forEach( (el) => {
-  el.style.background = bgAccent
-});
+  // Set BG Accent Color
+  document.querySelectorAll(".lesson-functions, .course-card, .gradient, .lesson-content__panel").forEach( (el) => {
+    el.style.background = bgAccent
+  });
 
-// Set Header Color
-document.querySelectorAll("h1, h3, h4, h5, h6, .internal-link, .nav-link, .lesson-navigation__link, .course-card-header__title, .viking-advert__brand-name, .viking-advert__pitch, .footer-link").forEach( (el) => {
-  el.style.color = headerColor 
-})
-document.querySelectorAll(".lesson-navigation__circle").forEach( (el) => {
-  el.style.background = headerColor
-})
+  // Set Header Color
+  document.querySelectorAll("h1, h3, h4, h5, h6, .internal-link, .nav-link, .lesson-navigation__link, .course-card-header__title, [href]:not([class]):hover, .viking-advert__brand-name, .viking-advert__pitch, .footer-link").forEach( (el) => {
+    el.style.color = headerColor 
+  })
+  document.querySelectorAll(".lesson-navigation__circle").forEach( (el) => {
+    el.style.background = headerColor
+  })
 
-// Set Font Color
+  // Set Font Color
+  document.querySelectorAll("p, ol, li").forEach( (el) => {
+    el.style.color = fontColor
+  })
+}
 
-document.querySelectorAll("p, ol, li").forEach( (el) => {
-  el.style.color = fontColor
-})
+window.addEventListener('load', darkMode, false);
 
+// Load with Turbolinks
+document.addEventListener('turbolinks:load', darkMode, false);
